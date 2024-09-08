1f11b69a7dbb8f3ae299dbf40a6530b0b9c7dda8
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-09-08 20:52:52 +0200

    replaced the template with my scriptg

--- express/server.js
+++ express/server.js
@@ -4,19 +4,41 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const { Rembg } = require('@xixiyahaha/rembg-node');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+router.post('/', async (req, res) => {
+  try {
+    if (!req.body.image) {
+      return res.status(400).send('No image data provided');
+    }
+
+    const base64Data = req.body.image.replace(/^data:image\/\w+;base64,/, "");
+    const buffer = Buffer.from(base64Data, 'base64');
+
+    const input = sharp(buffer);
+
+    const rembg = new Rembg({
+      logging: true,
+    });
+
+    const output = await rembg.remove(input);
+
+    const processedBuffer = await output.webp().toBuffer();
+
+    const processedBase64 = processedBuffer.toString('base64');
+
+    res.json({ image: `data:image/webp;base64,${processedBase64}` });
+  } catch (error) {
+    console.error('Error processing image:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
